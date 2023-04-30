1aff1ff295f0555c03c2b7fc32d9d2e47d9901d1
Berkan <berkan1458@gmail.com>
2023-04-30 06:44:50 +0300

    updated planet-details page

--- pages/planet-details/[name].tsx
+++ pages/planet-details/[name].tsx
@@ -6,10 +6,17 @@ const PlanetDetailsPage = () => {
   const router = useRouter();
   const { name } = router.query;
 
+  const handleBackClick = () => {
+    router.push('/planets');
+  };
+
   return (
     <Layout>
       <h1 className="text-2xl font-semibold mb-4">Planet Details</h1>
       <p>Planet Name: {name}</p>
+      <button onClick={handleBackClick} className="mt-4 bg-white text-2xl font-semibold">
+        Go Back
+      </button>
     </Layout>
   );
 };
