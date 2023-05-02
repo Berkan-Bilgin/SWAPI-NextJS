76024afdef06df796052b611ab8d63be93cafed3
Berkan <berkan1458@gmail.com>
2023-05-02 07:13:03 +0300

    updated starship detail

--- pages/starship-details/[id].tsx
+++ pages/starship-details/[id].tsx
@@ -1,13 +1,45 @@
 import React from 'react';
 import StarshipsDetailsPage from '@/components/StarshipDetailsPage';
-import { useRouter } from 'next/router';
+import { GetServerSideProps, NextPage } from 'next';
+import { ParsedUrlQuery } from 'querystring';
 
-const StarshipDetails = ({ starship }) => {
+interface Starship {
+  name: string;
+  model: string;
+  manufacturer: string;
+  cost_in_credits: string;
+  length: string;
+  max_atmosphering_speed: string;
+  crew: string;
+  passengers: string;
+  cargo_capacity: string;
+  consumables: string;
+  hyperdrive_rating: string;
+  MGLT: string;
+  starship_class: string;
+  pilots: string[];
+  films: string[];
+  created: string;
+  edited: string;
+  url: string;
+}
+
+interface StarshipDetailsProps {
+  starship: Starship;
+}
+
+const StarshipDetails: NextPage<StarshipDetailsProps> = ({ starship }) => {
   return <StarshipsDetailsPage starship={starship} />;
 };
 
-export async function getServerSideProps(context) {
-  const starshipId = context.params.id;
+export const getServerSideProps: GetServerSideProps<StarshipDetailsProps, ParsedUrlQuery> = async (context) => {
+  const starshipId = context.params?.id;
+
+  if (!starshipId) {
+    return {
+      notFound: true
+    }
+  }
 
   // SWAPI URL'sinde name değil, ID kullanıldığı için URL'yi ID'ye göre düzenliyoruz
   const starshipUrl = `https://swapi.dev/api/starships/${starshipId}/`;
