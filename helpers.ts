ff1fc7f985f1dacf6c27bd448e688cb263f2d037
Berkan <berkan1458@gmail.com>
2023-05-02 06:50:09 +0300

    updated interfaces

--- helpers.ts
+++ helpers.ts
@@ -1,6 +1,6 @@
 import imageData from './imageData';
 
-export const getStarshipImageByName = (name) => {
+export const getStarshipImageByName = (name:any) => {
   const starship = imageData.find((ship) => ship.name.toLowerCase() === name.toLowerCase());
   return starship ? starship.img : null;
 };
