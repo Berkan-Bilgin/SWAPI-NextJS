e5cfb62e93171f237277ed17bef4101f6716a5a5
Berkan Bilgin <berkan.1458@gmail.com>
2023-05-03 15:15:44 +0300

    updated format

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,13 +1,12 @@
-import React from 'react';
+import React from "react";
 
 const Footer: React.FC = () => {
   return (
     <footer className="footer-background h-60 text-white flex items-end justify-center py-6 relative ">
-       <div className="absolute inset-0 bg-gradient-to-br from-transparent via-transparent to-black opacity-100"></div>
-       <div className="absolute inset-0 bg-gradient-to-tl from-transparent via-transparent to-black opacity-100"></div>
-       <div className="absolute inset-0 bg-gradient-to-tr from-transparent via-transparent to-black opacity-100"></div>
-       <div className="absolute inset-0 bg-gradient-to-bl from-transparent via-transparent to-black opacity-100"></div>
-      
+      <div className="absolute inset-0 bg-gradient-to-br from-transparent via-transparent to-black opacity-100"></div>
+      <div className="absolute inset-0 bg-gradient-to-tl from-transparent via-transparent to-black opacity-100"></div>
+      <div className="absolute inset-0 bg-gradient-to-tr from-transparent via-transparent to-black opacity-100"></div>
+      <div className="absolute inset-0 bg-gradient-to-bl from-transparent via-transparent to-black opacity-100"></div>
     </footer>
   );
 };
