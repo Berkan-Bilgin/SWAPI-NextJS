d552348be954d0e36f2b2d8ae3faac6824a48ca2
Berkan <berkan1458@gmail.com>
2023-05-22 03:02:16 +0300

    added infinite scroll page

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -9,13 +9,18 @@ const data = [
   // { title: "Vehicles" },
   { title: "Starships" },
   { title: "Starshipstry" },
+  { title: "Infinite Scroll" },
 ];
 
 const Sidebar = () => {
   return (
     <div className="sm:w-100 md:w-36 lg:w-52 sticky top-28 mx-4">
       {data.map((item, index) => (
-        <Link href={`/${item.title.toLowerCase()}`} key={index}>
+        <Link
+        //Sayfa adında boşluk varsa boşluk yerine - yazılıyor.
+          href={`/${item.title.toLowerCase().replace(/\s/g, "-")}`}
+          key={index}
+        >
           <div className="flex justify-between items-center py-2 px-4 rounded-lg bg-red-800 font-bold text-gray-900 mb-3 cursor-pointer">
             <span>{item.title}</span>
           </div>
---
+++ pages/infinite-scroll.tsx
@@ -0,0 +1,14 @@
+import Layout from '@/components/Layout'
+import React from 'react'
+
+const Infinitescroll = () => {
+  return (
+    <Layout>
+        <div>infinite-scroll</div>
+
+    </Layout>
+    
+  )
+}
+
+export default Infinitescroll
